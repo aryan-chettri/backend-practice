015ba999ab736c472e942a846a4b3c1f8e888dbd
Aryan Chettri <aryanchettri17@gmail.com>
2025-05-01 16:36:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res)=>{
+
+    res.send('Hello world')
+})
+
+app.get('/information', (req, res)=>{
+
+    res.send('This is the information page')
+})
+
+app.listen(process.env.PORT, ()=> {
+
+    console.log(`The app is successfully running in Port: ${process.env.PORT}`)
+})
